698e820950bb8135f6215faadc070cfff2a050a7
narendra <dnarendrareddy1997@gmail.com>
2020-06-12 12:24:48 +0530

    changed sr files, added reusable button components

--- one-helloworld/src/App.js
+++ one-helloworld/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./style.css"
+import Button from "./Button"
+const App = () => {
+    return (
+        <div>
+        <h1 style={{textAlign:"center"}}>Hello World!!!</h1>
+        <Button title="App Store"/>
+        <Button title="Play Store"/>
+        {/* <button className="button">App Store</button>
+        <button className="button">Play Store</button> */}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+        </div>
+        );
+    };
+export default App
