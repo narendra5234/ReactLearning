52218fc131fe57b7687f20e9232074276e200f52
narendra <dnarendrareddy1997@gmail.com>
2020-06-26 18:35:16 +0530

    imlemented react apis

---
+++ myrepos-rest-api/src/App.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import List from './components/List';
+import withListLoading from './components/withListLoading';
+import axios from 'axios'
+
+
+function App() {
+  const ListLoading = withListLoading(List);
+
+  const [appState, setAppState] = useState({
+    loading: false,
+    repos: null,
+  });
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = 'https://api.github.com/users/narendra5234/repos';
+    axios.get(apiUrl).then((repos) => {
+      const allRepos = repos.data;
+      setAppState({ loading: false, repos: allRepos });
+    });
+
+  }, [setAppState]);
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1>My Repositories</h1>
+      </div>
+      <div className='repo-container'>
+        <ListLoading isLoading={appState.loading} repos={appState.repos} />
+      </div>
+      <footer>
+        <div className='footer'>
+          Built{' '}
+          <span role='img' aria-label='love'>
+            💚
+          </span>{' '}
+          with by Narendra Reddy
+        </div>
+      </footer>
+    </div>
+  );
+}
+export default App;
